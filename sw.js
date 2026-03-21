4c40115ba5f3094f26b8312aa18e6eaa45b7f73b
Nzoko Julien <nzokojulien0@gmail.com>
2026-03-21 02:47:26 +0100

    update

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
 self.addEventListener("install", e => {
-    console.log("Install!");
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll([
+                "./",
+                "./src/master.css",
+                "./images/icon.png"
+            ])
+        })
+    )
+})
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request)
+        })
+    )
 })
